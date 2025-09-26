6c0c5e7abc402489cfae8d67577f49fd10682a6e
Prabowo Wahyu Sudarno <PRABOWOWahyu45@gmail.com>
2025-09-26 09:06:07 +0000

    masih belum ya? masih hilang tanggalnya

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -35,7 +35,7 @@ function Calendar({
         table: "w-full border-collapse space-y-1",
         head_row: "flex",
         head_cell:
-          "text-muted-foreground rounded-md w-full font-normal text-[0.8rem]",
+          "text-muted-foreground rounded-md flex-1 font-normal text-[0.8rem]",
         row: "flex w-full mt-2",
         cell: "flex-1 h-9 text-center text-sm p-0 relative first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
         day: cn(
