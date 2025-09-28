4226e8ea5288588fbfe903db5d3fc58d35c3e97e
Prabowo Wahyu Sudarno <PRABOWOWahyu45@gmail.com>
2025-09-28 03:13:15 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -54,6 +54,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'upload.wikimedia.org',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.talenta.co',
+      },
     ],
   },
 };
